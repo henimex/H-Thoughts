25dd62dfab200735967f1455576be6739fdd3e99
henimex <49072861+henimex@users.noreply.github.com>
2022-04-26 13:43:17 +0300

    User Auth Operations

--- app/src/main/java/com/henimex/h_thoughts/MainActivity.kt
+++ app/src/main/java/com/henimex/h_thoughts/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,36 +21,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = Firebase.auth
+        val currentUser = auth.currentUser
+        checkLoggedUser(currentUser)
     }
 
-    fun login(view: View) {
-        val email = idText.text.toString();
-        val password = pwText.text.toString();
-
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    Toast.makeText(baseContext, "Logged In" + user.toString(), Toast.LENGTH_SHORT)
-                        .show();
-
-                    val intent = Intent(this, UserPanel::class.java);
-                    startActivity(intent);
-                    finish();
-                }
-                else {
-                    Toast.makeText(baseContext, "Login Failed", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(baseContext, task.exception.toString(), Toast.LENGTH_LONG)
-                        .show();
-                }
-            }
-            .addOnFailureListener { exception ->
-                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_SHORT).show();}
+    private fun checkLoggedUser(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            val intent = Intent(this, UserPanel::class.java);
+            startActivity(intent)
+            finish();
+        }
     }
 
     fun register(view: View) {
-        val email = idText.text.toString();
-        val password = pwText.text.toString();
+        val email = txt_id.text.toString();
+        val password = txt_pw.text.toString();
+        val userName = txt_userName.text.toString();
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -56,6 +44,21 @@ class MainActivity : AppCompatActivity() {
                     val user = auth.currentUser;
                     Toast.makeText(baseContext, "User Registration Completed", Toast.LENGTH_SHORT)
                         .show();
+
+                    val userProfile = userProfileChangeRequest {
+                        displayName = userName;
+                    }
+
+                    user?.updateProfile(userProfile)?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                baseContext,
+                                "Display Name Updated " + user?.displayName,
+                                Toast.LENGTH_SHORT
+                            )
+                                .show();
+                        }
+                    };
 
                     val intent = Intent(this, UserPanel::class.java);
                     startActivity(intent);
@@ -69,4 +72,37 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    fun login(view: View) {
+        val email = txt_id.text.toString();
+        val password = txt_pw.text.toString();
+
+        if (password != "" && email != "") {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(
+                            baseContext,
+                            "Logged In " + user?.email.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show();
+
+                        val intent = Intent(this, UserPanel::class.java);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(baseContext, "Login Failed", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(baseContext, task.exception.toString(), Toast.LENGTH_LONG)
+                            .show();
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(
+                        applicationContext,
+                        exception.localizedMessage,
+                        Toast.LENGTH_SHORT
+                    ).show();
+                }
+        }
+    }
 }

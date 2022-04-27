4741c6b40fbd36ed8f2cd5de328a38b83f85c653
henimex <49072861+henimex@users.noreply.github.com>
2022-04-27 16:58:25 +0300

    Storage Image Uri Fails

--- app/src/main/java/com/henimex/h_thoughts/adapter/UserPanelAdapter.kt
+++ app/src/main/java/com/henimex/h_thoughts/adapter/UserPanelAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.henimex.h_thoughts.R
 import com.henimex.h_thoughts.model.PostModel
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recycler_row.view.*
 
 class UserPanelAdapter(val downloadedPostList: ArrayList<PostModel>) :
@@ -23,6 +24,15 @@ class UserPanelAdapter(val downloadedPostList: ArrayList<PostModel>) :
     override fun onBindViewHolder(holder: PostHolder, position: Int) {
         holder.itemView.recycler_row_user_name.text = downloadedPostList[position].user;
         holder.itemView.recycler_row_post_msg.text = downloadedPostList[position].thoughts;
+        holder.itemView.recycler_row_post_uri.text = downloadedPostList[position].pictureUrl;
+        if (downloadedPostList[position].thoughts == null){
+            println("pictureUrl")
+        }
+        if (downloadedPostList[position].pictureUrl != null){
+            holder.itemView.rc_imageView.visibility = View.VISIBLE
+            Picasso.get().load(downloadedPostList[position].pictureUrl).into(holder.itemView.rc_imageView)
+            println(downloadedPostList[position].pictureUrl)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/henimex/h_thoughts/view/PrePostActivity.kt
+++ app/src/main/java/com/henimex/h_thoughts/view/PrePostActivity.kt
@@ -1,22 +1,37 @@
 package com.henimex.h_thoughts.view
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.henimex.h_thoughts.R
 import kotlinx.android.synthetic.main.activity_pre_post.*
+import java.util.*
 
 class PrePostActivity : AppCompatActivity() {
 
     val colName = "SharedPosts"
     val db = Firebase.firestore
+    val storage = Firebase.storage
     private lateinit var auth: FirebaseAuth
+    var selectedPicture: Uri? = null;
+    var selectedBitmap: Bitmap? = null;
     //private lateinit var rdb : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,18 +47,119 @@ class PrePostActivity : AppCompatActivity() {
         val user = auth.currentUser!!.displayName.toString();
         val postDate = Timestamp.now();
 
-        val postData = hashMapOf<String, Any>()
-        postData.put("thoughts", thoughts)
-        postData.put("user", user)
-        postData.put("post_date", postDate)
+        if (selectedPicture != null){
+            val uuid = UUID.randomUUID()
+            val imageName = "${uuid}.jpg"
+            val reference = storage.reference
+            val imageRef = reference.child("images").child(imageName)
+            imageRef.putFile(selectedPicture!!).addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val takenUri = uri.toString()
+                    val postData = hashMapOf<String, Any>()
+                    postData["thoughts"] = thoughts
+                    postData["user"] = user
+                    postData["post_date"] = postDate
+                    postData["pictureUrl"] = takenUri
 
-        db.collection(colName).add(postData).addOnCompleteListener { task ->
-            if (task.isSuccessful){
-                Toast.makeText(this,"Post Added", Toast.LENGTH_LONG).show()
-                finish();
+                    db.collection(colName).add(postData).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Post Added", Toast.LENGTH_LONG).show()
+                            finish();
+                        }
+                    }.addOnFailureListener { exception ->
+                        Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
             }
-        }.addOnFailureListener { exception ->
-            Toast.makeText(this,exception.localizedMessage, Toast.LENGTH_LONG).show()
+        } else {
+            val postData = hashMapOf<String, Any>()
+            postData["thoughts"] = thoughts
+            postData["user"] = user
+            postData["post_date"] = postDate
+            postData["pictureUrl"] = "noUri"
+
+            db.collection(colName).add(postData).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Post Added", Toast.LENGTH_LONG).show()
+                    finish();
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    fun saveImageToStorage() {
+        if (selectedPicture != null){
+            val uuid = UUID.randomUUID()
+            val imageName = "${uuid}.jpg"
+            val reference = storage.reference
+            val imageRef = reference.child("images").child(imageName)
+            imageRef.putFile(selectedPicture!!).addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val takenUri = uri.toString()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun uploadImage(view: View) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            //request perm
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        } else {
+            //perm granted go gallery
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, 2)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent, 2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            selectedPicture = data.data;
+
+            if (selectedPicture != null) {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    val source = ImageDecoder.createSource(this.contentResolver, selectedPicture!!)
+                    selectedBitmap = ImageDecoder.decodeBitmap(source)
+                    imageView.setImageBitmap(selectedBitmap)
+                } else {
+                    selectedBitmap =
+                        MediaStore.Images.Media.getBitmap(this.contentResolver, selectedPicture)
+                    imageView.setImageBitmap(selectedBitmap)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
